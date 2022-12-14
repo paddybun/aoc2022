4e10bc0c25d485730c2e0f24cb00fb0a438d5a3a
Patrick Bunda <1834214+paddybun@users.noreply.github.com>
2022-12-14 15:10:47 +0100

    feat: parameter reordering

--- AdventOfCode2022/Day6.cs
+++ AdventOfCode2022/Day6.cs
@@ -11,18 +11,18 @@ public class Day6
         var chars = File.ReadAllLines("./d6.txt").First().ToCharArray().ToList();
 
         Console.WriteLine("\r\n----- Day 06 -----");
-        Console.WriteLine($"Solution 01: {LookAhead(0, 1, chars, new List<char>(), 3)}");
-        Console.WriteLine($"Solution 02: {LookAhead(0, 1, chars, new List<char>(), 13)}");
+        Console.WriteLine($"Solution 01: {LookAhead(0, 1, 3, chars, new List<char>())}");
+        Console.WriteLine($"Solution 02: {LookAhead(0, 1, 13, chars, new List<char>())}");
     }
 
-    private int LookAhead(int toCheckIndex, int checkDepth, List<char> charlist, List<char> checkedChars, int maxCheckDepth)
+    private int LookAhead(int toCheckIndex, int checkDepth, int maxCheckDepth, List<char> charlist, List<char> checkedChars)
     {
         var currentChar = charlist[toCheckIndex];
         var charToCompare = charlist[toCheckIndex + checkDepth];
         if (currentChar != charToCompare && checkDepth != maxCheckDepth && !checkedChars.Contains(charToCompare))
         {
             checkedChars.Add(charToCompare);
-            return LookAhead(toCheckIndex, ++checkDepth, charlist, checkedChars, maxCheckDepth);
+            return LookAhead(toCheckIndex, ++checkDepth, maxCheckDepth, charlist, checkedChars);
         }
 
         if (currentChar != charToCompare && checkDepth == maxCheckDepth && !checkedChars.Contains(charToCompare))
@@ -30,6 +30,6 @@ public class Day6
             return (toCheckIndex + 1) + checkDepth;
         }
 
-        return LookAhead(++toCheckIndex, 1, charlist, new List<char>(), maxCheckDepth);
+        return LookAhead(++toCheckIndex, 1, maxCheckDepth, charlist, new List<char>());
     }
 }
